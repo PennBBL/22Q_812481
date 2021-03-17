ccce04af887625e662d8337c76edbd1eac166e37
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-03-17 15:09:27 -0400

    Script to run single subject through Freesurfer v7.1.1 via singularity

---
+++ scripts/preproc/freesurfer_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+'''
+ssh singularity01
+
+# run from  project/bbl_projects/22Q dir
+singularity exec --cleanenv \
+'''
+
+unset SINGULARITYENV_TMPDIR
+export SINGULARITYENV_FS_LICENSE=~/22Q/images/license.txt
+export SINGULARITYENV_SUBJECTS_DIR=~/22Q/data/freesurfer
+export SINGULARITYENV_INPUT_DIR=~/22Q/data/bids_directory/sub-17867/ses-22Q1/anat
+
+recon-all -subjid sub-17867 -i $INPUT_DIR/sub-17867_ses-22Q1_T1w.nii.gz -all
+
