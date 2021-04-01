b4b92118cd3c2e43c27d686d842f236ad8185203
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-04-01 13:51:43 -0400

    Modified to run FS on all subjects. Creates FS jobscript for each subject and submits to bsub.

--- scripts/preproc/freesurfer_run.sh
+++ scripts/preproc/freesurfer_run.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-unset SINGULARITYENV_TMPDIR
-export SINGULARITYENV_FS_LICENSE=/project/bbl_projects/22Q/images/license.txt
-export SINGULARITYENV_SUBJECTS_DIR=/project/bbl_projects/22Q/data/freesurfer
+module load freesurfer/7.1.1
+export FREESURFER_HOME=/appl/freesurfer-7.1.1
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-singularity exec --cleanenv  /project/bbl_projects/22Q/images/freesurfer_7.1.1.sif \
-recon-all -s sub-17867 -i /project/bbl_projects/22Q/data/bids_directory/sub-17867/ses-22Q1/anat/sub-17867_ses-22Q1_T1w.nii.gz -all
+export LOGS_DIR=/home/kzoner/logs/fs
+
+project=/project/bbl_projects/22Q
+input_dir=${project}/data/bids_directory
+output_dir=${project}/data/freesurfer-7.1.1
+
+mkdir -p ${project}/scripts/jobscripts/fs_jobscripts
+
+imgList=`ls $input_dir/*/*/anat/*.nii.gz`
+
+for img in $imgList; do
+	
+	subj=`basename $img | cut -d _ -f 1`
+	sess=`basename $img | cut -d _ -f 2`
+	echo SUBJECT: $subj SESSION: $sess
+	
+	subDir=$output_dir/$subj
+	mkdir -p $subDir
+
+	jobscript=${project}/scripts/jobscripts/fs_jobscripts/${subj}.sh
+	
+	cat <<- EOS > ${jobscript}
+		#!/bin/bash
+		$FREESURFER_HOME/bin/recon-all -i $img -sd $subDir -s $sess -all
+	EOS
+
+	bsub -e $LOGS_DIR/${subj}.e -o $LOGS_DIR/${subj}.o -q bbl_normal -m linc1 ${jobscript}
+	break
+done
+
 
