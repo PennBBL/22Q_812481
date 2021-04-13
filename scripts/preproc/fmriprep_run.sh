4924db5615c072e13cf6ffffa8b921d1c23d0556
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-04-13 15:57:45 -0400

    Added binding of SINGULARITY_TMPDIR --> fixes nu_correct error.

--- scripts/preproc/fmriprep_run.sh
+++ scripts/preproc/fmriprep_run.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 project=/project/bbl_projects/22Q
+LOGS=/home/kzoner/logs/22Q/fmriprep
+
 mkdir -p ${project}/scripts/jobscripts/fmriprep_jobscripts
 
 SIF=${project}/images/fmriprep_20.2.1.sif
 
-subjList=`ls ${project}/data/bids_directory/`
+subjList=`ls -d ${project}/data/bids_directory/sub-* | cut -d / -f 7`
 
 for subject in $subjList; do
 
 	echo SUBJECT: ${subject}
+	jobscript=${project}/scripts/jobscripts/fmriprep_jobscripts/${subject}.sh
 
-	cat <<- EOS > ${project}/scripts/jobscripts/fmriprep_jobscripts/${subject}.sh
+	cat <<- EOS > ${jobscript}
 		#!/bin/bash
 		
+		SINGULARITYENV_SURFER_FRONTDOOR=1 \\
 		singularity run --cleanenv \\
 		-B ${project}/data/bids_directory:/bids \\
 		-B ${project}/data/preproc:/out \\
-		-B ${project}/images/license.txt:/license/txt \\
+		-B ${project}/images/license.txt:/license.txt \\
 		-B ${project}/work:/work \\
+		-B ${SINGULARITY_TMPDIR}:${SINGULARITYENV_TMPDIR} \\
 		${SIF} \\
 		/bids /out participant \\
 		--participant-label ${subject} \\
@@ -28,5 +33,9 @@ for subject in $subjList; do
 		--skip-bids-validation
 		
 		EOS
+
+	chmod +x ${jobscript}
+	bsub -e ${LOGS}/${subject}.e -o ${LOGS}/${subject}.o ${jobscript}
+	break
 done
 
